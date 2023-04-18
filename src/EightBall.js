35d5a52ef18ad078e2c2e5d38963d613e35323a5
Terry Li <terryseanli@gmail.com>
2023-04-18 12:08:47 -0700

    added code review #1 comments

--- src/EightBall.js
+++ src/EightBall.js
@@ -13,6 +13,8 @@ import React, { useState } from "react";
 */
 
 function EightBall({ answers }) {
+  // TODO: could have one piece of state storing both msg and color
+  // const [answer, setAnswer] = useState({msg: "Think of...", color: "black"});
   const [msg, setMsg] = useState("Think of a Question.");
   const [color, setColor] = useState("black");
 
@@ -22,25 +24,29 @@ function EightBall({ answers }) {
     const answer = answers[Math.floor(Math.random() * answers.length)];
     setMsg(answer.msg);
     console.log("msg:", msg);
+
     setColor(answer.color);
     console.log("color:", color);
   }
 
-  console.log("After handleClick", msg, color);
-
+  // TODO: put this in a .css file and convert property names to .css format
   const buttonStyle = {
     color: "white",
     backgroundColor: color,
     width: "200px",
+    fontSize: "14pt",
     height: "200px",
     borderRadius: "50%"
   };
 
+  // TODO: style={{backgroundColor: color}}, put rest of style in .css
   return (
+    <div>
     <button onClick={handleClick} style={buttonStyle}>
       <b>{msg}</b>
     </button>
-  )
+  </div>
+  );
 }
 
 export default EightBall;
